4795d79e9dd9f0ef225482a49e66483c0acce576
HossElbosss <84256353+HossElbosss@users.noreply.github.com>
2022-08-16 17:42:54 -0700

    crossOriginAdded

--- src/main/java/com/UserLog/User/controller/UserController.java
+++ src/main/java/com/UserLog/User/controller/UserController.java
@@ -10,14 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.UserLog.User.entity.User;
 import com.UserLog.User.service.UserService;
@@ -26,6 +19,7 @@ import com.UserLog.User.service.UserService;
 
 @RestController
 @RequestMapping(path="user/v1")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
     @Autowired
     private UserService userService;
